fe1a9758ecdf89ad049b9d33f44177c5fee9a9b4
Thyago Thayllan <thayllanst@outlook.com>
2025-06-26 22:55:38 -0300

    Subir alterações de estudo.

--- contact/admin.py
+++ contact/admin.py
@@ -4,4 +4,12 @@ from .models import Contact
 
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
-    ...
+    list_filter = ('created_at',)
+
+    list_display = ('id', 'first_name', 'email', 'phone', 'created_at',)
+
+    ordering = ('-created_at',)
+
+    search_fields = ('id', 'first_name', 'email', 'phone',)
+
+    list_per_page = 25
--- contact/models.py
+++ contact/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 
 
+class Category(models.Model):
+    name = models.CharField('Categoria', max_length=50)
+
+
 class Contact(models.Model):
+    category = models.ForeignKey(Category, blank=True, null=True, on_delete=models.SET_NULL)
+
     created_at = models.DateTimeField('Data de Criação', auto_now_add=True)
 
     description = models.TextField('Descrição', blank=True)
@@ -10,9 +16,13 @@ class Contact(models.Model):
 
     first_name = models.CharField('Nome', max_length=50)
 
+    image = models.ImageField('Imagem', blank=True, upload_to='images/contact/%Y/%m/')
+
     last_name = models.CharField('Sobrenome', blank=True, max_length=50)
 
     phone = models.CharField('Telefone', max_length=50)
+
+    show = models.BooleanField(default=True)
 
     def __str__(self) -> str:
         return f'{self.id} | {self.first_name} | {self.phone}'

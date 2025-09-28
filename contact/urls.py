e69b7327c866b4e602c8ee60f820fc3fee1579f0
Thyago Thayllan <thayllanst@outlook.com>
2025-09-28 17:17:32 -0300

    subir alterações

--- contact/urls.py
+++ contact/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from contact.views import ContactCreate
+from contact.views import ContactUpdate
 from contact.views import ContactView
 from contact.views import Contacts
 
@@ -9,5 +10,6 @@ app_name = 'contact'
 urlpatterns = [
     path('', Contacts.as_view(), name='contacts'),
     path('contact/<int:pk>/', ContactView.as_view(), name='contact'),
+    path('contact/<int:pk>/update/', ContactUpdate.as_view(), name='update'),
     path('contact/create/', ContactCreate.as_view(), name='create'),
 ]
--- contact/views.py
+++ contact/views.py
@@ -1,10 +1,11 @@
 from django.contrib import messages
 from django.core.paginator import Paginator
 from django.db.models import Q
-from django.http import HttpRequest
+from django.http import HttpRequest, HttpResponseRedirect
 from django.http.response import HttpResponse
 from django.shortcuts import redirect
 from django.shortcuts import render
+from django.urls import reverse
 from django.views.generic import TemplateView
 
 from contact.forms import ContactForm
@@ -12,28 +13,84 @@ from contact.models import Contact
 
 
 class ContactCreate(TemplateView):
-    template_name = 'contact/contact-create.html'
+    template_name = 'contact/contact-form.html'
+    title = 'Criar Contato'
 
     def get(self, request: HttpRequest) -> HttpResponse:
-        context = {'form': ContactForm()}
+        action_form = reverse('contact:create')
+
+        context = {'action_form': action_form, 'form': ContactForm(), 'title': self.title}
 
         return render(request, self.template_name, context)
 
     def post(self, request: HttpRequest) -> HttpResponse:
         form = ContactForm(data=request.POST)
 
+        if form.is_valid():
+            contact = Contact.objects.create(**form.cleaned_data)
+
+            return redirect(reverse('contact:update', kwargs={'pk': contact.pk}))
+
+        return render(request, self.template_name, {'form': form})
+
+
+class ContactUpdate(TemplateView):
+    template_name = 'contact/contact-form.html'
+    title = 'Editar Contato'
+
+    def get(self, request: HttpRequest, pk: int) -> HttpResponse | HttpResponseRedirect:
+        try:
+            contact = Contact.objects.get(pk=pk)
+        except Contact.DoesNotExist:
+            breakpoint()
+            print('ollaaaaaaaaaaaaaaaaa')
+            messages.error(request, f'Contato de ID <b>{pk}</b> não existe. Tente novamente.')
+            return redirect('contact:contacts')
+
+        print('ollaaaaaaaaaaaaaaaaa 2222222222')
+        action_form = reverse('contact:update', kwargs={'pk': pk})
+
+        form = ContactForm(instance=contact)
+
+        context = {
+            'action_form': action_form,
+            'contact': contact,
+            'form': form,
+            'title': self.title,
+        }
+
+        return render(request, self.template_name, context)
+
+    def post(self, request: HttpRequest, pk: int) -> HttpResponse | HttpResponseRedirect:
+        form = ContactForm(data=request.POST)
+
         if not form.is_valid():
             return render(request, self.template_name, {'form': form})
 
-        Contact.objects.create(**form.cleaned_data)
+        form.save()
 
-        return redirect('contact:create')
+        return redirect('contact:update')
+
+
+class ContactView(TemplateView):
+    template_name = 'contact/contact.html'
+
+    def get(self, request: HttpRequest, pk: int) -> HttpResponse | HttpResponseRedirect:
+        try:
+            contact = Contact.objects.get(pk=pk)
+        except Contact.DoesNotExist:
+            messages.error(request, f'Contato de ID <strong>"{pk}"</strong> não encontrado.')
+            return redirect('contact:contacts')
+
+        context = {'contact': contact, 'title': contact.first_name}
+
+        return render(request, self.template_name, context)
 
 
 class Contacts(TemplateView):
     template_name = 'contact/contacts.html'
 
-    def get(self, request):
+    def get(self, request: HttpRequest) -> HttpResponse:
         search_value = request.GET.get('search')
 
         contacts = None
@@ -58,20 +115,5 @@ class Contacts(TemplateView):
         page_obj = paginator.get_page(page_number)
 
         context = {'page_obj': page_obj, 'title': 'Contatos'}
-
-        return render(request, self.template_name, context)
-
-
-class ContactView(TemplateView):
-    template_name = 'contact/contact.html'
-
-    def get(self, request, pk):
-        try:
-            contact = Contact.objects.get(pk=pk)
-        except Contact.DoesNotExist:
-            messages.error(request, f'Contato de ID <strong>"{pk}"</strong> não encontrado.')
-            return redirect('contact:contacts')
-
-        context = {'contact': contact, 'title': contact.first_name}
 
         return render(request, self.template_name, context)
